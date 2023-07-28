a3353c1dfc67bd2a9a2cc3d7b4dbaa10cd732986
rs028 <rob.sommariva@gmail.com>
2023-07-28 12:58:37 +0100

    Collate all output for each room

--- MBM_extractor.py
+++ MBM_extractor.py
@@ -24,10 +24,10 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with INCHEM-Py.  If not, see <https://www.gnu.org/licenses/>.
 
-This script inputs the out_data.pickle files from model runs of INCHEM-Py
-from specified output directories, saves and plots the species given in
-species_to_plot in a csv and a png respectively. These are saved in the
-output_folder
+This script inputs the out_data.pickle files from model runs of MBM-Flex
+from specified output directories, sand aves the species given in
+species_to_plot in a csv file for each room. The csv files are saved in the
+extracted_outputs folder .
 """
 
 # Import modules
@@ -37,41 +37,41 @@ import pandas as pd
 # =============================================================================================== #
 # Variables to change
 
-main_output_dir = '20230726_145738_Test_Serial'
-nroom = 3     # number of rooms
-nchem = 10
+main_output_dir = '20230727_191734_TestSerial'   # folder with all outputs of the model run
+nroom = 3    # number of rooms
 
-#species to extract and plot
-species_to_extract=['O3','OH_reactivity','OH_production','J1']
-#All species will be saved to a separate csv for each input directory.
-#A maximum of three separate graphs will be made; species concentrations,
-#reactivity, and production.
+total_seconds_to_integrate = 7200   # total duration of the model run (see `settings_serial.py`)
+tchem_only = 300   # duration of chemistry-only integrations (see `settings_serial.py`)
+nchem = int(total_seconds_to_integrate/tchem_only)
 
-# output_folder to save the csv files. Can already exist or it will be created.
+# Model variables to extract
+species_to_extract=['O3','NO','NO2','OH_reactivity','J4']
+
+# The extracted_outputs folder where the csv files are saved is inside the main_output_dir
 output_folder = ('%s/extracted_outputs' % main_output_dir)
 
 # =============================================================================================== #
-# Extract the data and save to csv files
+# Extract the selected variables and save them to one csv files per room
 
 for iroom in range(0,nroom):
 
-    #directories of data to extract and plot
+    # directories of data to extract
     out_directories = []
     for ichem in range (0,nchem):
         out_directories.append('%s/%s_%s' % (main_output_dir,'room{:02d}'.format(iroom+1),'c{:04d}'.format(ichem)))
 
-    #define dictionary of output dataframes from out_directories
+    # dictionary of output dataframes from out_directories
     out_data = {}
     for i in out_directories:
         with open('%s/out_data.pickle' % i, 'rb') as handle:
             out_data[i] = pd.read_pickle(handle)
 
-    #get the current working directory and create the output folder if it doesn't exist  
+    # create the extracted_outputs folder if it doesn't exist
     path=os.getcwd()
     if not os.path.exists('%s/%s' % (path,output_folder)):
         os.mkdir('%s/%s' % (path,output_folder))
 
-    #create and save csvs
-    for i in out_data:
-        out_data[i].to_csv('%s/%s/%s.csv' % (path,output_folder,'room{:02d}'.format(iroom+1)),
-                           columns=species_to_extract)
+    # join all output for a room and save it to the corresponding csv file
+    out_merged = pd.concat(out_data.values())
+    out_merged.to_csv('%s/%s/%s_%s.csv' % (path,output_folder,main_output_dir,'room{:02d}'.format(iroom+1)),
+                      columns=species_to_extract, index_label='Time')
