106ec2554b171aa3c1a39876c0301263310f4ce0
rs028 <rob.sommariva@gmail.com>
2023-08-17 13:53:06 +0100

    Tweak plotting tool

--- MBM_plots.R
+++ MBM_plots.R
@@ -6,7 +6,7 @@ library(ggplot2)
 library(scales)
 
 ## set directory of model run outputs
-main.output <- "20230727_191734_TestSerial"
+main.output <- "20230816_125453_TestSerial"
 setwd(main.output)
 
 ## set filename of pdf file for plots
@@ -14,7 +14,7 @@ pdfname <- "mbmflex_output.pdf"
 
 ## ----------------------------------------
 output.dir <- "extracted_outputs/"
-output.files <- list.files(output.dir)
+output.files <- list.files(output.dir, pattern=".csv")
 
 # number of rooms
 nroom <- length(output.files) - 1
@@ -24,7 +24,7 @@ indoor.df <- data.frame()
 for (n in 1:nroom) {
   fname <- paste0(main.output, sprintf("_room%02d",n), ".csv")
   ind.df <- read.csv(paste0(output.dir, fname), header=TRUE)
-  ind.df$ROOM <- paste0("R", n)
+  ind.df$ROOM <- paste("Room", n, sep=" ")
   indoor.df <- rbind(indoor.df, ind.df)
 }
 
@@ -48,28 +48,35 @@ who.df <- cbind(who.aer, (who.gas*0.5*2.46e10))
 
 ## plot model output and save to pdf file
 pdf(paste0(output.dir, pdfname), paper="a4r", width=0, height=0)
-
 room.list <- unique(indoor.df$ROOM)
 vars.list <- names(indoor.df)
 for (i in 2:(length(vars.list)-1)) {
   vars <- vars.list[i]
-  p <- ggplot(data=indoor.df, aes(x=Time, y=.data[[vars]], color=ROOM, name="")) +
-    geom_line(linewidth=2) + labs(color=guide_legend(title=""))
-  ## plot ambient concentrations (if available)
+  gp <- ggplot(data=indoor.df, aes(x=Time, y=.data[[vars]], color=ROOM, name="")) +
+               geom_line(linewidth=2) + labs(color=guide_legend(title=""))
+  gp.col <- hue_pal()(nroom)
+  gp.lab <- unique(room.list)
+  ## plot outdoor concentrations (if available)
   vars.out <- paste0(vars, "OUT")
   if(vars.out %in% names(outdoor.df)) {
-    p <- p + geom_line(data=outdoor.df, aes(x=Time, y=.data[[vars.out]], color="black"),
-                       linewidth=2, linetype="dashed") +
-      scale_color_manual(values=c("black", hue_pal()(nroom)), labels=c("Ambient",unique(room.list)))
+    gp <- gp + geom_line(data=outdoor.df, aes(x=Time, y=.data[[vars.out]], color="black"),
+                         linewidth=2, linetype="dotdash")
+    gp.col <- c("black", gp.col)
+    gp.lab <- c("Outdoors", gp.lab)
   }
+
   ## plot WHO guidelines (if available)
-  if (vars %in% names(who.df)) {
-    guidel <- who.df[[which(names(who.df)==vars)]]
-    p <- p + geom_hline(yintercept=guidel, linetype="dotted", color="black")
-  }
-  print(p)
+  # if (vars %in% names(who.df)) {
+  #   who.gl <- who.df[[which(names(who.df)==vars)]]
+  #   gp <- gp + geom_hline(yintercept=who.gl[1], linewidth=2, linetype="dotted", color="black")
+  #   gp <- gp + geom_hline(yintercept=who.gl[2], linewidth=2, linetype="dotted", color="black")
+  # }
+  gp.legend <- scale_color_manual(values=gp.col, labels=gp.lab)
+  gp <- gp + gp.legend
+  print(gp)
 }
 dev.off()
 
 ## return to MBM-Flex directory
+cat("\n*** Plots saved to", paste0(main.output,"/",output.dir,pdfname), "***\n")
 setwd("../")
